e5ae89fc924d7a862eabfa5f4fdf90c6f81a9acd
Sayan Bhattacharjee <7991sayan@gmail.com>
2024-04-13 14:58:41 +0530

    setup app.js , index.js, apierror.js,apiresponse.js,asyncHandler.js
    setup app.js , index.js, apierror.js,apiresponse.js,asyncHandler.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import dotenv from "dotenv";
 import connetDB from "./db/connetDB.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "/env",
 });
 
-connetDB();
+const port = process.env.PORT || 5000;
+connetDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Server error: ", err);
+      throw err;
+      //   process.exit(1);
+    });
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection failed:::", err);
+  });
