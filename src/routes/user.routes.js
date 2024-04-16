d50066b62df05f6fb658c6ddd3e7dca77cf620ec
Sayan Bhattacharjee <7991sayan@gmail.com>
2024-04-17 01:57:39 +0530

    auth middleware and logout api

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(
@@ -14,5 +19,8 @@ router.route("/register").post(
   registerUser
 );
 router.route("/login").post(loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJwt, logoutUser);
 
 export default router;
